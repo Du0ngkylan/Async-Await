77901a75800db21d1c293bfac35c59134befe138
Du0ngkylan <duongxuanmai.k55tt@gmail.com>
2019-02-14 14:35:56 +0700

    update

--- 02.js
+++ 02.js
@@ -1,11 +1,13 @@
+//                         NODEJS Xử lý bất đồng bộ – Bài 2: Ôn tập function trong javascript
+
 // Hàm higher-order và hàm callback là gì?
 
-// Hàm higher-order (higher-order function) là hàm có hoạt động dựa trên 1 hàm khác, tức là:
-// nó có thể nhận hàm (function) làm tham số đầu vào,
-// hoặc sẽ return ra 1 hàm khác. Một trong 2 điều kiện đó xảy ra thì được gọi là hàm higher-order.
+// - Hàm higher-order (higher-order function) là hàm có hoạt động dựa trên 1 hàm khác, tức là:
+//   nó có thể nhận hàm (function) làm tham số đầu vào,
+//   hoặc sẽ return ra 1 hàm khác. Một trong 2 điều kiện đó xảy ra thì được gọi là hàm higher-order.
 
-// Hàm callback làm hàm được truyền vào “hàm-khác” như một tham số đầu vào,
-// sau đó sẽ được gọi kích hoạt bên trong “hàm-khác” này.
+// - Hàm callback làm hàm được truyền vào “hàm-khác” như một tham số đầu vào,
+//   sau đó sẽ được gọi kích hoạt bên trong “hàm-khác” này.
 
 
 let mang = [5, 6, 1, 8, 9];
@@ -31,5 +33,3 @@ getFunction(1)();
 let a = () => {};
 
 console.log(a());
-
-
